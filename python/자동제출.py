81f43f5e8d09159bd716022677955d92ece857e4
mwy3055 <mwy3055@gmail.com>
2019-02-28 15:05:42 +0900

    3184 AC

--- python/자동제출.py
+++ python/자동제출.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 browser = webdriver.Chrome(executable_path=r"E:\chromedriver.exe")
 browser.get('https://www.acmicpc.net/login?next=%2F')
 ID = "isolet"
@@ -11,3 +12,4 @@ browser.find_element_by_xpath('/html/body/div[3]/div[3]/div/div/form/div[4]/div[
 while True:
   browser.get('https://www.acmicpc.net/submit/16972/11963170') # 이전에 제출한 곳의 주소
   browser.find_element_by_xpath('//*[@id="submit_form"]/div[4]/div/button').click()
+  time.sleep(9.5)
