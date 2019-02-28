4807ea84296ae5617d187dce825c0b124b898daf
mwy3055 <mwy3055@gmail.com>
2019-02-28 14:33:48 +0900

    3190 AC

---
+++ python/자동제출.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+browser = webdriver.Chrome(executable_path=r"E:\chromedriver.exe")
+browser.get('https://www.acmicpc.net/login?next=%2F')
+ID = "isolet"
+PW = "Isolet#3707"
+elem_login = browser.find_element_by_name("login_user_id")
+elem_login.send_keys(ID)
+elem_login = browser.find_element_by_name("login_password")
+elem_login.send_keys(PW)
+browser.find_element_by_xpath('/html/body/div[3]/div[3]/div/div/form/div[4]/div[2]/button').click()
+while True:
+  browser.get('https://www.acmicpc.net/submit/16972/11963170') # 이전에 제출한 곳의 주소
+  browser.find_element_by_xpath('//*[@id="submit_form"]/div[4]/div/button').click()
