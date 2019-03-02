c7cef0ede3ccb798139847af6e4a100fcf02dad1
mwy3055 <mwy3055@gmail.com>
2019-03-02 19:43:26 +0900

    14754 AC

---
+++ c++/14754.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int num[1000][1000];
+bool ismax[1000][1000] = {false};
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    long sum = 0, msum = 0;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        int midx = 0;
+        for (int j = 0; j < m; j++)
+        {
+            cin >> num[i][j];
+            sum += num[i][j];
+            if (num[i][midx] < num[i][j])
+                midx = j;
+        }
+        ismax[i][midx] = true;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int midx = 0;
+        for (int j = 1; j < n; j++)
+        {
+            if (num[midx][i] < num[j][i])
+                midx = j;
+        }
+        ismax[midx][i] = true;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (ismax[i][j])
+            {
+                msum += num[i][j];
+            }
+        }
+    }
+    cout << sum - msum;
+}
