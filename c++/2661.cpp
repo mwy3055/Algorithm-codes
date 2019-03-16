581ec03b02dafb36650120b82a4cdd51c5813c55
unknown <mwy3055@o365sen.net>
2019-03-16 15:33:18 +0900

    개발환경 구축

--- c++/2661.cpp
+++ c++/2661.cpp
@@ -2,7 +2,24 @@
 #include <string>
 using namespace std;
 
-bool iskind(string &s) {}
+bool iskind(string &s)
+{
+    if (s.length() == 0)
+        return true;
+
+    int len = s.length();
+    for (int i = 0; i < len; i++) //탐색 시작점
+    {
+        for (int j = 1; j < len; j++)
+        {
+            
+        }
+    }
+}
+
+string make(string &s, int &n) //길이 n의 좋은 수열을 만들어야 함
+{
+}
 
 int main()
 {
