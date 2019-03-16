2705b5cd023204b0692f35f7e3a457d3baffe16c
unknown <mwy3055@o365sen.net>
2019-03-16 15:24:47 +0900

    개발 환경 설정

---
+++ c++/2661.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool iskind(string &s) {}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+}
