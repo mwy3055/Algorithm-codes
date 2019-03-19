2f6850bac2f394efc4b69426902eca7ba7adfbb3
unknown <mwy3055@o365sen.net>
2019-03-19 17:28:35 +0900

    2602 AC(DP)

--- c++/2602.cpp
+++ c++/2602.cpp
@@ -16,19 +16,25 @@ int main()
 
     dpa[0][0] = (angel[0] == duru[0]);
     dpd[0][0] = (devil[0] == duru[0]);
-    for (int i = 1; i < brlen; i++)
+    for (int i = 1; i < brlen; i++) //돌다리의 i번 돌을 밟을 차례
     {
         dpa[i][0] = (angel[i] == duru[0]);
         dpd[i][0] = (devil[i] == duru[0]);
-        for (int k = 1; k < dulen; k++)
+        for (int k = 1; k < dulen; k++) //돌다리의 i번 글자와 두루마리의 k번 글자를 비교
         {
             if (angel[i] == duru[k])
-                dpa[i][k] += dpd[i - 1][k - 1];
+                for (int j = 0; j < i; j++)
+                    dpa[i][k] += dpd[j][k - 1];
             if (devil[i] == duru[k])
-                dpd[i][k] += dpa[i - 1][k - 1];
-            int a = 1;
+                for (int j = 0; j < i; j++)
+                    dpd[i][k] += dpa[j][k - 1];
         }
     }
 
-    cout << dpa[brlen - 1][dulen - 1] + dpd[brlen - 1][dulen - 1];
+    int sum = 0;
+    for (int i = 0; i < brlen; i++)
+    {
+        sum += dpa[i][dulen - 1] + dpd[i][dulen - 1];
+    }
+    cout << sum;
 }
