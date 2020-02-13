73edd4323f7bf04263a77b22db21336ca3007ec0
mwy3055 <mwy3055@gmail.com>
2020-02-13 14:26:55 +0900

    12852. 1로 만들기 2 - dp

---
+++ c++/12852.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[1000001], track[1000001];
+void solve(int &n)
+{
+    dp[1] = 0;
+    dp[2] = 1, track[2] = 2;
+    dp[3] = 1, track[3] = 1;
+    for (int i = 4; i <= n; i++)
+    {
+        if (i % 3 == 0)
+        {
+            dp[i] = min(dp[i / 3], dp[i - 1]) + 1;
+            track[i] = (dp[i / 3] < dp[i - 1] ? 1 : 3);
+        }
+        else if (i % 2 == 0)
+        {
+            dp[i] = min(dp[i / 2], dp[i - 1]) + 1;
+            track[i] = (dp[i / 2] < dp[i - 1] ? 2 : 3);
+        }
+        else
+        {
+            dp[i] = dp[i - 1] + 1;
+            track[i] = 3;
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    solve(n);
+
+    cout << dp[n] << '\n';
+    int cur = n;
+    do
+    {
+        cout << cur << ' ';
+        switch (track[cur])
+        {
+        case 1:
+            cur /= 3;
+            break;
+        case 2:
+            cur /= 2;
+            break;
+        default:
+            cur--;
+        }
+    } while (cur);
+}
