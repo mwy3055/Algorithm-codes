4a1128fa06157d9b08606330678a10ce5873250b
unknown <mwy3055@konkuk.ac.kr>
2020-02-19 16:53:03 +0900

    union find library

---
+++ my library/union_find.cpp
@@ -0,0 +1,29 @@
+const int MAX = 100000;
+
+int root[MAX + 1];
+
+void init()
+{
+    for (int i = 0; i <= MAX; i++)
+        root[i] = -1;
+}
+
+int find(int n)
+{
+    if (root[n] < n)
+        return n;
+    return root[n] = find(root[n]);
+}
+
+void union_vertex(int a, int b)
+{
+    int root1 = find(a), root2 = find(b);
+    if (root1 == root2)
+        return;
+
+    if (root[root1] > root[root2])
+        swap(root1, root2);
+
+    root[root1] += root[root2];
+    root[root2] = root1;
+}
