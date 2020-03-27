6a6311c7c7c652acba33f1195b714c1009fb2e2e
mwy3055 <mwy3055@gmail.com>
2020-03-27 15:14:15 +0900

    1722. 순열의 순서 - 수학

--- c++/1722.cpp
+++ c++/1722.cpp
@@ -1,55 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void make_nth(int &n)
+typedef long long ll;
+typedef vector<int> vi;
+ll fac[21];
+int n, arr[20];
+
+void init()
 {
-    int k;
-    cin >> k;
-
-    list<int> l;
-    for (int i = 1; i <= n; i++)
-    {
-        int ith = k % i;
-        k /= i;
-        list<int>::iterator it = l.begin();
-        while (ith--)
-            it++;
-        l.insert(it, i);
-    }
-
-    for (int &i : l)
-        cout << i << ' ';
+    fac[0] = 1;
+    for (int i = 1; i <= 20; i++)
+        fac[i] = fac[i - 1] * i;
 }
 
-void calculate_nth(int &n)
+void solve1(int len, ll k, vi &left, vi &ans)
 {
-    list<int> l;
-    for (int i = 0; i < n; i++)
+    if (len == 2)
     {
-        int t;
-        cin >> t;
-        l.push_back(t);
+        if (k == 1)
+        {
+            ans.push_back(left[0]);
+            ans.push_back(left[1]);
+        }
+        else
+        {
+            ans.push_back(left[1]);
+            ans.push_back(left[0]);
+        }
+        return;
     }
-
-    int m = 0;
-    for (int i = n; i >= 1; i--)
+    else if (len == 1)
     {
-        int ith = distance(find(l.begin(), l.end(), i), l.begin());
-        m = m * n + ith;
+        ans.push_back(left[0]);
+        return;
     }
+    if (len == 0)
+        return;
 
-    cout << m;
+    int cur = 1;
+    while (cur * fac[len - 1] <= k)
+    {
+        cur++;
+        if (cur * fac[len - 1] == k)
+            break;
+    }
+    ans.push_back(left[cur - 1]);
+    left.erase(left.begin() + cur - 1);
+    solve1(len - 1, k - (cur - 1) * fac[len - 1], left, ans);
 }
+ll solve2(int len, vi &left, vi &arr)
+{
+    if (len == n)
+        return 1;
 
+    auto it = find(left.begin(), left.end(), arr[len - 1]);
+    ll nth = it - left.begin() + 1;
+    left.erase(it);
+    return (nth - 1) * fac[n - len] + solve2(len + 1, left, arr);
+}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n, prob;
-    cin >> n >> prob;
-    if (prob == 1)
-        make_nth(n);
+    init();
+    cin >> n;
+    int type;
+    cin >> type;
+    vi left;
+    for (int i = 1; i <= n; i++)
+        left.push_back(i);
+    if (type == 1)
+    {
+        ll k;
+        vi ans;
+        cin >> k;
+        solve1(n, k, left, ans);
+        for (auto &a : ans)
+            cout << a << ' ';
+    }
     else
-        calculate_nth(n);
+    {
+        vi arr(n);
+        for (auto &a : arr)
+            cin >> a;
+        cout << solve2(1, left, arr);
+    }
 }
