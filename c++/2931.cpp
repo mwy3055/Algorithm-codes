52e09468d4c97ce18d39a947ad611e9c839701ba
mwy3055 <mwy3055@gmail.com>
2020-04-16 16:05:05 +0900

    2931. 가스관 - 구현

--- c++/2931.cpp
+++ c++/2931.cpp
@@ -1,107 +1,176 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
-typedef struct coord {
-	int y, x;
-} coord;
 
-enum direction
+struct node
 {
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT
+	int y, x, dir; // 아래의 인덱스를 따름
+	node(int y = 0, int x = 0, int dir = 0) : y(y), x(x), dir(dir) {}
 };
+const int INF = 99999;
+int r, c, cnt, src[][2] = {0, 1, 1, 0, 0, -1, -1, 0};
+char board[25][26], block[] = {'|', '-', '+', '1', '2', '3', '4'};
+node m, z;
 
-char map[25][25];
-int r, c, _left;
-direction dir;
-coord m, z, to_fill;
-
-inline bool isin(int i, int j)
+bool isin(int y, int x)
 {
-	return (0 <= i && i < r) && (0 <= j && j < c);
+	return 0 <= y && y < r && 0 <= x && x < c;
 }
-
-void getInput()
+bool isin(node &n)
 {
-	string rmv = ".MZ";
-	cin >> r >> c;
-	for (int i = 0; i < r; i++)
+	return 0 <= n.y && n.y < r && 0 <= n.x && n.x < c;
+}
+int rotate(int y, int x, int dir)
+{
+	if (board[y][x] == '|' && dir % 2)
+		return dir;
+	else if (board[y][x] == '-' && dir % 2 == 0)
+		return dir;
+	else if (board[y][x] == '+')
+		return dir;
+	else if (board[y][x] == '1' && dir == 3)
+		return 0;
+	else if (board[y][x] == '1' && dir == 2)
+		return 1;
+	else if (board[y][x] == '2' && dir == 1)
+		return 0;
+	else if (board[y][x] == '2' && dir == 2)
+		return 3;
+	else if (board[y][x] == '3' && dir == 1)
+		return 2;
+	else if (board[y][x] == '3' && dir == 0)
+		return 3;
+	else if (board[y][x] == '4' && dir == 0)
+		return 1;
+	else if (board[y][x] == '4' && dir == 3)
+		return 2;
+	return INF;
+}
+void start(node &n)
+{
+	auto &y = n.y, &x = n.x, &dir = n.dir;
+	dir = -1;
+	for (int i = 0; i < 4; i++)
 	{
-		string temp;
-		cin >> temp;
-		for (int j = 0; j < c; j++)
+		int ny = y + src[i][0], nx = x + src[i][1];
+		if (isin(ny, nx) && board[ny][nx] != '.')
 		{
-			map[i][j] = temp.at(j);
-			if (rmv.find(map[i][j]) == rmv.npos)
-				_left++;
-			if (map[i][j] == 'M')
-				m = { i,j };
-			else if (map[i][j] == 'Z')
-				z = { i,j };
+			dir = i;
+			return;
 		}
 	}
 }
-
-bool is_correct(coord now)
+bool cango(node &cur, int &left)
 {
-	int left = _left;
-
-
-
-
-
-
-
-
-
-
-	return ((left == 0) ? (true) : (false));
-}
-bool start()
-{
-	if (isin(m.y + 1, m.x) && map[m.y + 1][m.x] == '|')
-		dir = UP;
-	else if (isin(m.y - 1, m.x) && map[m.y - 1][m.x] == '|')
-		dir = DOWN;
-	else if (isin(m.y, m.x + 1) && map[m.y][m.x + 1] == '-')
-		dir = RIGHT;
-	else if (isin(m.y, m.x - 1) && map[m.y][m.x - 1] == '-')
-		dir = LEFT;
-	else
-		return false;
-	return true;
-}
-void search()
-{
-	int src[4][2] = { {1,0},{-1,0},{-1,0},{1,0} };
-	coord now = m;
-	
-	if (!start())
+	while (isin(cur))
 	{
-		//주변 4칸에 블럭을 넣고 테스트해봐야함
-		for (int i = 0; i < 4; i++)
+		if (board[cur.y][cur.x] == '.')
+			return false;
+		auto &dir = cur.dir;
+		cur.y += src[dir][0], cur.x += src[dir][1];
+		if (board[cur.y][cur.x] == 'Z')
+			return true;
+		auto ndir = rotate(cur.y, cur.x, dir);
+		if (ndir == INF)
+			return false;
+		dir = ndir;
+		left--;
+	}
+	return left <= 0;
+}
+
+bool canstart(node &cur) // (y, x)에서 dir 방향으로 갈 수 있는지
+{
+	auto &dir = cur.dir;
+	int ny = cur.y + src[dir][0], nx = cur.x + src[dir][1];
+	if (board[ny][nx] == '|' && dir % 2)
+		return true;
+	else if (board[ny][nx] == '-' && dir % 2 == 0)
+		return true;
+	else if (board[ny][nx] == '+')
+		return true;
+	else if (board[ny][nx] == '1' && (dir == 2 || dir == 3))
+		return true;
+	else if (board[ny][nx] == '2' && (dir == 1 || dir == 2))
+		return true;
+	else if (board[ny][nx] == '3' && (dir == 0 || dir == 1))
+		return true;
+	else if (board[ny][nx] == '4' && (dir == 0 || dir == 3))
+		return true;
+	return false;
+}
+void solve1(node cur)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		auto test = cur;
+		int ny = test.y + src[i][0], nx = test.x + src[i][1];
+		test.dir = i;
+		if (isin(ny, nx))
 		{
-			int sy = m.y + src[i][0], sx = m.x + src[i][1];
-			if (isin(sy, sx))
+			for (int b = 0; b < 7; b++)
 			{
-				
+				board[ny][nx] = block[b];
+				if (canstart(test) && cango(test, cnt))
+				{
+					cout << ny + 1 << ' ' << nx + 1 << ' ' << block[b];
+					return;
+				}
 			}
 		}
 	}
-
-
 }
+void solve2(node cur)
+{
+	int count = cnt;
+	cango(cur, count);
 
+	int ny = cur.y, nx = cur.x;
+	cur.y -= src[cur.dir][0], cur.x -= src[cur.dir][1];
+	for (int b = 0; b < 7; b++)
+	{
+		board[ny][nx] = block[b];
+		if (canstart(cur))
+		{
+			auto test = cur;
+			auto tcount = count;
+			if (cango(test, tcount))
+			{
+				cout << ny + 1 << ' ' << nx + 1 << ' ' << block[b];
+				return;
+			}
+		}
+	}
+}
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cout.tie(0);
 
-	getInput();
-	search();
+	cin >> r >> c;
+	for (int i = 0; i < r; i++)
+	{
+		cin >> board[i];
+		for (int j = 0; j < c; j++)
+		{
+			if (board[i][j] == 'M')
+				m = node(i, j);
+			else if (board[i][j] == 'Z')
+				z = node(i, j);
+			if (board[i][j] != 'M' && board[i][j] != 'Z' && board[i][j] != '.')
+				cnt++;
+		}
+	}
 
-	cout << to_fill.y << " " << to_fill.x << " " << dir;
+	node cur = m;
+	start(cur);
+	if (cur.dir == -1)
+	{
+		// 시작점에서 나갈 수 없음
+		solve1(cur);
+	}
+	else
+	{
+		// 일단 출발해 보자
+		solve2(cur);
+	}
 }
