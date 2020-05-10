33f445a01d05d967b48affed2e5692c70f087bbc
mwy3055 <mwy3055@gmail.com>
2020-05-10 11:49:46 +0900

    ccw library update

--- my library/ccw.cpp
+++ my library/ccw.cpp
@@ -2,7 +2,7 @@ int ccw(pair<int, int> x, pair<int, int> y, pair<int, int> z) // (x, y)
 {
     int v1x = y.first - x.first, v1y = y.second - x.second;
     int v2x = z.first - y.first, v2y = z.second - y.second;
-    int cross = v1x * v2y - v1y * v2x;
+    int cross = v1x * v2y - v1y * v2x; // overflow!!
     if (cross > 0)
         return 1;
     else if (cross < 0)
