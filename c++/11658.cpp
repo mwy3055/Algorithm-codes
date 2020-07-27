6283de5d33823d2f7b515b6aa84298fa57b11264
mwy3055 <mwy3055@gmail.com>
2020-07-27 17:30:13 +0900

    11658. 구간 합 구하기 3 - 펜윅 트리

--- c++/11658.cpp
+++ c++/11658.cpp
@@ -1,86 +1,39 @@
 #include <bits/stdc++.h>
 
 using vi = std::vector<int>;
+std::vector<vi> tree(1025, vi(1025, 0));
+int n;
 
-struct segment
+int sum(int x, int y) // (1, 1) ~ (x, y)
 {
-    int x, y, len, val = 0;
-};
-const int MAX_DEP = 22, MAX = (1 << MAX_DEP) - 1, IDENTITY = 0;
-segment tree[MAX];
-int n, d[][2] = {0, 0, 0, 1, 1, 0, 1, 1};
-
-bool isin(int x, int y, int len, int tx, int ty)
-{
-    return x <= tx && tx <= x + len - 1 && y <= ty && ty <= y + len - 1;
-}
-
-int maketree(int idx, int x, int y, int len, std::vector<vi> &arr)
-{
-    auto &now = tree[idx];
-    now.x = x, now.y = y, now.len = len;
-    if (len == 1)
+    int ret = 0;
+    while (x > 0)
     {
-        if (x < n && y < n)
-            now.val = arr[x][y];
-        else
-            now.val = IDENTITY;
-    }
-    else
-    {
-        int nlen = len / 2;
-        for (int i = 0; i < 4; i++)
+        int ty = y;
+        while (ty > 0)
         {
-            int result = maketree(4 * idx + (i + 1), x + nlen * d[i][0], y + nlen * d[i][1], nlen, arr);
-            now.val += result;
+            ret += tree[x][ty];
+            ty -= (ty & -ty);
         }
+        x -= (x & -x);
     }
-    return now.val;
+    return ret;
 }
-
-int updateval(int idx, int &tx, int &ty, int &val)
+int getsum(int x1, int y1, int x2, int y2)
 {
-    auto &x = tree[idx].x, &y = tree[idx].y, &len = tree[idx].len, &cval = tree[idx].val;
-    if (x == tx && y == ty && len == 1)
-    {
-        cval = val;
-    }
-    else if (!isin(x, y, len, tx, ty))
-    {
-        ;
-    }
-    else
-    {
-        int rtn = 0;
-        for (int i = 0; i < 4; i++)
-        {
-            int result = updateval(4 * idx + (i + 1), tx, ty, val);
-            rtn += result;
-        }
-        cval = rtn;
-    }
-    return cval;
+    return sum(x2, y2) - sum(x1 - 1, y2) - sum(x2, y1 - 1) + sum(x1 - 1, y1 - 1);
 }
-int getval(int idx, int sx, int sy, int ex, int ey)
+void update(int x, int y, int diff)
 {
-    auto &x = tree[idx].x, &y = tree[idx].y, &len = tree[idx].len;
-    if (sx <= x && x + len - 1 <= ex && sy <= y && y + len - 1 <= ey)
+    while (x <= n)
     {
-        return tree[idx].val;
-    }
-    else if ((ex < x || ey < y) || (x + len - 1 < sx || y + len - 1 < sy))
-    {
-        return IDENTITY;
-    }
-    else
-    {
-        int rtn = 0;
-        for (int i = 1; i <= 4; i++)
+        int ty = y;
+        while (ty <= n)
         {
-            int result = getval(4 * idx + i, sx, sy, ex, ey);
-            rtn += result;
+            tree[x][ty] += diff;
+            ty += (ty & -ty);
         }
-        return rtn;
+        x += (x & -x);
     }
 }
 
@@ -91,11 +44,15 @@ int main()
 
     int m;
     std::cin >> n >> m;
-    std::vector<vi> arr(n, vi(n));
-    for (auto &row : arr)
-        for (auto &col : row)
-            std::cin >> col;
-    maketree(0, 0, 0, 1024, arr);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            int num;
+            std::cin >> num;
+            update(i, j, num);
+        }
+    }
 
     for (int i = 0; i < m; i++)
     {
@@ -105,13 +62,13 @@ int main()
         {
             int x, y, c, target;
             std::cin >> x >> y >> c;
-            updateval(0, --x, --y, c);
+            update(x, y, c - getsum(x, y, x, y));
         }
         else
         {
             int x1, y1, x2, y2;
             std::cin >> x1 >> y1 >> x2 >> y2;
-            std::cout << getval(0, --x1, --y1, --x2, --y2) << '\n';
+            std::cout << getsum(x1, y1, x2, y2) << '\n';
         }
     }
 }
