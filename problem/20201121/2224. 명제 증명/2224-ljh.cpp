dbdb9d2847b8db8df31e861674b865988fa2b0cc
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2020-11-22 21:50:32 +0900

    ljh- accept code commit

--- problem/20201121/2224. 명제 증명/2224-ljh.cpp
+++ problem/20201121/2224. 명제 증명/2224-ljh.cpp
@@ -1,34 +1,19 @@
 
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
+const int INF = 0x3f3f3f3f;
+
+const int MAX = 52;
 int n;
 char x, y;
 string tmp; //버리는 문자열 
-int graph[55][55];
 
-void floydWarshall(void)
-{
-	for (int k = 0; k <= 51; k++)
-		for (int i = 0; i <= 51; i++)
-			for (int j = 0; j <= 51; j++)
-				//1->1 이런거 어차피 0이라 
-				if (i == j)
-					continue;
-				else if (graph[i][k] && graph[k][j])
-					//경로가 존재한다면 true 아니면 false
-				{
-					if (graph[i][j] == 0)
-						//한번엔 못가도 경유노드를 거쳐 탐색이 가능할때
-						graph[i][j] = graph[i][k] + graph[k][j];
-					
-					else
-						//핵심! 더 빠른 경로로 갱신 
-						graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
-					
-				}
-}
+int graph[MAX+1][MAX+1];
+
 
 int ctoi(char c) { return c>='A' && c<='Z'?c-'A':c-'a'+26; }
 char itoc(int n) { return n<26?'A'+n:'a'+n-26; }
@@ -39,6 +24,8 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    memset(graph, INF, sizeof(graph));
+
     cin >> n;
 
     for(int i = 0; i < n; i++){
@@ -46,21 +33,32 @@ int main()
         graph[ctoi(x)][ctoi(y)] = 1;
     }
 
-    floydWarshall();
+    // Floyd-warshall
+    for (int k = 0; k < 52; k++)
+    {
+        for (int i = 0; i < 52; i++)
+        {
+            for (int j = 0; j < 52; j++)
+            {
+                if (i != j && j != k && k != i)
+                    graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+            }
+        }
+    }
 
     int cnt = 0;
 
-    for(int i = 0; i <= 51; i++){
-        for(int j = 0; j <= 51; j++){
-            if(graph[i][j] != 0) 
+    for(int i = 0; i < 52; i++){
+        for(int j = 0; j < 52; j++){
+            if(i != j  && graph[i][j] != INF) 
                 cnt++;
         }
     }
     cout << cnt << '\n';
 
-     for(int i = 0; i <= 51; i++){
-        for(int j = 0; j <= 51; j++){
-            if(graph[i][j] != 0)
+     for(int i = 0; i < 52; i++){
+        for(int j = 0; j < 52; j++){
+            if(i != j && graph[i][j] != INF)
                 cout << itoc(i) << " => " << itoc(j) << '\n';
         }
     }
