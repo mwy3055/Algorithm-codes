0dfe51bd275cf3983fe9308cb7d258c575c43710
mwy3055 <mwy3055@gmail.com>
2020-12-14 19:11:43 +0900

    fix: make code more general

--- my library/segment_tree.cpp
+++ my library/segment_tree.cpp
@@ -4,27 +4,27 @@ typedef struct S
     int seg_s, seg_e;
     ll val;
 } Segment;
-const int MAX_DEP = 17, IDENTITY = 0; // n <= 2^MAX_DEP
+const int MAX_DEP = 17, IDENTITY = 0; // n <= 2^(MAX_DEP-1)
 const int MAX = (1 << MAX_DEP) - 1;
 Segment tree[MAX];
 
 // sum segment tree
-void make_tree(int i, int n, ll *arr)
+void make_tree(int i, std::vector<ll> &arr)
 {
     auto &now = tree[i];
     if ((1 << (MAX_DEP - 1)) - 1 <= i)
     {
         int idx = i - ((1 << (MAX_DEP - 1)) - 1);
         now.seg_s = now.seg_e = idx;
-        if (idx < n)
+        if (idx < arr.size())
             now.val = arr[idx];
         else
-            now.val = 0;
+            now.val = IDENTITY;
     }
     else
     {
-        make_tree(2 * i + 1, n, arr);
-        make_tree(2 * i + 2, n, arr);
+        make_tree(2 * i + 1, arr);
+        make_tree(2 * i + 2, arr);
         now.seg_s = tree[2 * i + 1].seg_s, now.seg_e = tree[2 * i + 2].seg_e;
         now.val = tree[2 * i + 1].val + tree[2 * i + 2].val;
     }
@@ -33,7 +33,7 @@ ll solve(int i, int s, int e)
 {
     auto &now = tree[i];
     if (e < now.seg_s || now.seg_e < s)
-        return 0;
+        return IDENTITY;
     else if (s <= now.seg_s && now.seg_e <= e)
         return now.val;
     else
@@ -43,9 +43,7 @@ ll change(int i, int &idx, int &val)
 {
     auto &now = tree[i];
     if (now.seg_s == idx && now.seg_e == idx)
-    {
         return now.val = val;
-    }
     else if (now.seg_s == now.seg_e || idx < now.seg_s || now.seg_e < idx)
         return now.val;
     else
