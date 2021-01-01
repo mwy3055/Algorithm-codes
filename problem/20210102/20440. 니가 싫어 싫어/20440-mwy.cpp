f50a5c25cbb26158ca03f764316141cac8652213
mwy3055 <mwy3055@gmail.com>
2021-01-01 15:59:56 +0900

    mwy answer code

---
+++ problem/20210102/20440. 니가 싫어 싫어/20440-mwy.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using pii = std::pair<int, int>;
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int n;
+    std::cin >> n;
+    std::vector<pii> times;
+    for (int i = 0; i < n; i++)
+    {
+        int s, e;
+        std::cin >> s >> e;
+        times.emplace_back(s, e);
+    }
+    std::sort(times.begin(), times.end());
+
+    auto comp = [](const pii &a, const pii &b) {
+        return a.second > b.second;
+    };
+    std::priority_queue<pii, std::vector<pii>, decltype(comp)> pq(comp);
+    int ans = 0, em = 0, xm = (1LL << 31) - 1;
+    for (auto &time : times)
+    {
+        int lasttime = -1;
+        while (!pq.empty() && pq.top().second <= time.first)
+        {
+            lasttime = pq.top().second;
+            pq.pop();
+        }
+        pq.push(time);
+        if (ans < pq.size())
+        {
+            ans = pq.size();
+            em = time.first;
+            xm = std::min(pq.top().second, time.second);
+        }
+        else if (ans == pq.size())
+        {
+            if (lasttime == time.first)
+                xm = time.second;
+        }
+    }
+    std::cout << ans << '\n'
+              << em << ' ' << xm;
+}
