2cd15862e0715975ddcd73e2fb6f2d6518dcce0e
hsk <mwy3055@gmail.com>
2021-03-18 20:16:21 +0900

    9527. 1의 개수 세기 - 수학

--- c++/9527.cpp
+++ c++/9527.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 
 using ll = long long;
-const ll MAX = (1LL << 61) - 1;
 
 ll mylog2(ll i)
 {
@@ -14,24 +13,6 @@ ll mylog2(ll i)
     return log - 1;
 }
 
-ll min(ll i)
-{
-    return 1LL << i;
-}
-
-ll max(ll i)
-{
-    return (1LL << (i + 1)) - 1;
-}
-
-// a보다 크거나 같으면서 가장 작은 n의 배수
-ll lower(ll a, ll n)
-{
-    if (a % n == 0)
-        return a;
-    return n * (a / n + 1);
-}
-
 // b보다 작거나 같으면서 가장 큰 n의 배수
 ll upper(ll b, ll n)
 {
@@ -40,97 +21,31 @@ ll upper(ll b, ll n)
     return n * (b / n);
 }
 
-// 2^i ~ 2^(i+1)-1
-ll sq2(ll i)
-{
-    if (i == 0)
-        return 1;
-    return i * (1LL << (i - 1)) + (1 << i);
-}
-
-ll count(ll n)
-{
-    int ans = 0;
-    while (n)
-    {
-        ans += n % 2;
-        n /= 2;
-    }
-    return ans;
-}
-
-ll naive(ll a, ll b)
-{
-    ll ans = 0;
-    for (ll i = a; i <= b; i++)
-        ans += count(i);
-    return ans;
-}
-
-/**
- * a와 b를 이진수로 나타냈을 때의 공통부분을 제거할 수 있는 bitmask를 반환
- */
-ll get_mask(ll a, ll b)
-{
-    ll sq = mylog2(a);
-    while ((a & (1LL << sq)) == (b & (1LL << sq)))
-    {
-        sq--;
-    }
-    return (1LL << (sq + 1)) - 1;
-}
-
 ll solve(ll, ll);
 
-// log2(left) = log2(right)인 left와 right만 주어짐
-ll calc(ll left, ll right)
+// sum(f(i)) for 1 <= i <= n
+ll calc(ll n)
 {
-    if (left == right)
-        return count(left);
-    else if (left > right)
+    if (n <= 0)
         return 0;
-    ll log = mylog2(left), ans = 0;
-    if (left == min(log) && right == max(log))
-        return sq2(log);
-    // 켜져 있는 i번째 비트의 개수 세기
-    for (ll i = 0; i < log; i++)
+    ll log = mylog2(n), rtn = 0;
+    for (ll i = 0; i <= log; i++)
     {
         ll sq = 1LL << i;
-        ll left_bound = lower(left, sq), right_bound = upper(right, sq);
-        if (left_bound > right || right_bound < left)
-        {
-            ans += ((left & sq) != 0) * (right - left + 1);
-            continue;
-        }
-        // [left, left_bound)
-        ans += ((left & sq) != 0) * (left_bound - left);
-        // [left_bound, right_bound)
-        ll seg_num = (right_bound - left_bound) / sq;
-        ll first_count = seg_num / 2 + seg_num % 2, second_count = seg_num / 2;
-        ll first_ans = ((left_bound & sq) != 0) * first_count * sq, second_ans = (((left_bound + sq) & sq) != 0) * second_count * sq;
-        ans += first_ans + second_ans;
-        // [right_bound, right]
-        ans += ((right & sq) != 0) * (right - right_bound + 1);
+        // 구간별
+        ll len = sq, right_bound = upper(n, sq); // 구간의 길이와 끝 (exclusive)
+        ll seg_count = right_bound / len;
+        ll one_count = seg_count / 2;
+        rtn += one_count * len;
+        // 자투리
+        rtn += ((n & sq) != 0) * (n - right_bound + 1);
     }
-    // 항상 켜져 있는 최상위 비트 세기
-    return ans + (right - left + 1);
+    return rtn;
 }
 
 ll solve(ll a, ll b)
 {
-    ll ans = 0, log_a = mylog2(a);
-    ll log_max = max(log_a);
-    ll left = a, right = std::min(b, log_max);
-    do
-    {
-        right = std::min(b, log_max);
-        ll tans = calc(left, right);
-        ans += tans;
-        assert(tans >= 0);
-        log_max = (log_max << 1) + 1;
-        left = right + 1;
-    } while (right < b);
-    return ans;
+    return calc(b) - calc(a - 1);
 }
 
 int main()
@@ -140,5 +55,5 @@ int main()
 
     ll a, b;
     std::cin >> a >> b;
-    std::cout << solve(a, b) << ' ' << naive(a, b) << '\n';
+    std::cout << solve(a, b) << '\n';
 }
