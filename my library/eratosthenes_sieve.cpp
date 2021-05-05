01aed80590053a840b46fc7e913043d22c85b361
hsk <mwy3055@gmail.com>
2021-05-05 10:42:40 +0900

    library update: 에라토스테네스의 체

--- my library/eratosthenes_sieve.cpp
+++ my library/eratosthenes_sieve.cpp
@@ -1,21 +1,17 @@
-const int n = 100000;
-bool isprime[n + 1];
-int smallest_pf[n + 1]; // i의 가장 작은 소인수
-int count_pf[n + 1];    // i의 서로 다른 소인수의 개수
-
-void init()
-{
-    std::memset(isprime, true, n + 1);
-}
+const int MAX = 100000;
+bool isprime[MAX + 1];
+int smallest_pf[MAX + 1]; // i의 가장 작은 소인수
+int count_pf[MAX + 1];    // i의 서로 다른 소인수의 개수
 
 void calculate()
 {
+    std::memset(isprime, true, MAX + 1);
     isprime[0] = isprime[1] = false;
-    for (int i = 2; i <= n; i++)
+    for (int i = 2; i <= MAX; i++)
     {
         if (isprime[i])
         {
-            for (int p = i * i; p <= n; p += i)
+            for (int p = i * i; p <= MAX; p += i)
             {
                 isprime[p] = false;
                 if (!smallest_pf[p])
