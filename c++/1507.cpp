c7579011a1aabf50c27b0a425ffd565c89b77742
hsk <mwy3055@gmail.com>
2021-06-26 23:24:35 +0900

    업데이트된 union find를 적용

--- c++/1507.cpp
+++ c++/1507.cpp
@@ -5,36 +5,34 @@ using pii = std::pair<int, int>;
 const int INF = 0x3f3f3f3f;
 int n, original[20][20], d[20][20];
 
-// Union-find begin
-const int MAX = 20;
-int root[MAX + 1];
-
-void init()
+class UnionFind
 {
-    for (int i = 0; i <= MAX; i++)
-        root[i] = -1;
-}
+private:
+    int size;
+    std::vector<int> root;
 
-int find(int n)
-{
-    if (root[n] < 0)
-        return n;
-    return root[n] = find(root[n]);
-}
-
-void union_vertex(int a, int b)
-{
-    int root1 = find(a), root2 = find(b);
-    if (root1 == root2)
-        return;
-
-    if (root[root1] > root[root2])
-        std::swap(root1, root2);
-
-    root[root1] += root[root2];
-    root[root2] = root1;
-}
-// Union-find end
+public:
+    UnionFind(int size) : size(size)
+    {
+        root.resize(size + 1, -1);
+    }
+    int find(int n)
+    {
+        if (root[n] < 0)
+            return n;
+        return root[n] = find(root[n]);
+    }
+    void union_node(int a, int b)
+    {
+        int root1 = find(a), root2 = find(b);
+        if (root1 == root2)
+            return;
+        if (root1 > root2)
+            std::swap(root1, root2);
+        root[root1] += root[root2];
+        root[root2] = root1;
+    }
+};
 
 // start, dest 사이의 거리
 int get_dist(int start, int dest)
@@ -69,6 +67,7 @@ int get_dist(int start, int dest)
 
 int solve(std::vector<std::pair<int, pii>> &edges)
 {
+    auto union_find = UnionFind(n);
     int ans = 0;
     std::sort(edges.begin(), edges.end());
     // connect with greedy
@@ -81,7 +80,7 @@ int solve(std::vector<std::pair<int, pii>> &edges)
         {
             ans += cost;
             d[n1][n2] = d[n2][n1] = cost;
-            union_vertex(n1, n2);
+            union_find.union_node(n1, n2);
         }
     }
 
@@ -103,8 +102,6 @@ int main()
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
     std::memset(d, -1, sizeof(d));
-
-    init();
 
     std::vector<std::pair<int, pii>> edges;
 
