b0b4b04566ebab24abc416a38b3eae9d1ef2aaf1
Wooyoung Myung <mwy3055@gmail.com>
2022-09-05 15:16:07 +0900

    주석 추가

--- baekjoon/cpp/c++/17387.cpp
+++ baekjoon/cpp/c++/17387.cpp
@@ -3,10 +3,13 @@ using namespace std;
 using ll = long long;
 typedef pair<int, int> pii;
 
-int ccw(pair<int, int> x, pair<int, int> y, pair<int, int> z) // (x, y)
+int ccw(pii x, pii y, pii z) // (x, y)
 {
+    // 벡터 XY와 벡터 YZ를 외적하자.
     ll v1x = y.first - x.first, v1y = y.second - x.second;
     ll v2x = z.first - y.first, v2y = z.second - y.second;
+    // 외적 벡터의 Z축 성분
+    // 양수: 반시계, 음수: 시계, 0: 한 직선 위에 존재
     ll cross = v1x * v2y - v1y * v2x;
     if (cross > 0)
         return 1;
@@ -15,10 +18,13 @@ int ccw(pair<int, int> x, pair<int, int> y, pair<int, int> z) // (x, y)
     else
         return 0;
 }
-bool meet_segment(pair<int, int> a, pair<int, int> b, pair<int, int> c, pair<int, int> d) // (x1, y1) ~ (x2, y2), (x3, y3) ~ (x4, y4)
+bool meet_segment(pii a, pii b, pii c, pii d) // (x1, y1) ~ (x2, y2), (x3, y3) ~ (x4, y4)
 {
+    // 선분 AB를 기준으로 C와 D가 반대인가?
     int ab = ccw(a, b, c) * ccw(a, b, d);
+    // 선분 CD를 기준으로 A와 B가 반대인가?
     int cd = ccw(c, d, a) * ccw(c, d, b);
+    // 한 직선 위에 있다면
     if (ab == 0 && cd == 0)
     {
         if (a > b)
@@ -27,6 +33,8 @@ bool meet_segment(pair<int, int> a, pair<int, int> b, pair<int, int> c, pair<int
             swap(c, d);
         return c <= b && a <= d;
     }
+    // 둘 다 반대여야 교차
+    // 등호 빼면 안 된다!
     return ab <= 0 && cd <= 0;
 }
 int main()
