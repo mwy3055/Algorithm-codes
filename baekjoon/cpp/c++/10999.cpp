5e64a23da4cbd2adda94afe0ceba3498850499f1
Wooyoung Myung <mwy3055@gmail.com>
2022-10-11 13:44:38 +0900

    10999. 구간 합 구하기 2 - 펜윅 트리 (구간 업데이트)

--- baekjoon/cpp/c++/10999.cpp
+++ baekjoon/cpp/c++/10999.cpp
@@ -1,27 +1,40 @@
 #include <bits/stdc++.h>
 
 using ll = long long;
-ll tree[1000001];
+const int MAX = 1000000;
+ll mul[MAX + 1], add[MAX + 1];
+
 int n, m, k;
 
-void update(int idx, int diff)
+void update_tree(int idx, ll dmul, ll dadd)
 {
     while (idx <= n)
     {
-        tree[idx] += diff;
+        mul[idx] += dmul;
+        add[idx] += dadd;
         idx += (idx & -idx);
     }
 }
+
+void update(int l, int r, ll d)
+{
+    update_tree(l, d, -d * (l - 1));
+    update_tree(r + 1, -d, d * r);
+}
+
 ll sum(int e)
 {
-    ll ret = 0;
-    while (e > 0)
+    ll smul = 0, sadd = 0;
+    int i = e;
+    while (i > 0)
     {
-        ret += tree[e];
-        e -= (e & -e);
+        smul += mul[i];
+        sadd += add[i];
+        i -= (i & -i);
     }
-    return ret;
+    return smul * e + sadd;
 }
+
 ll sum(int s, int e)
 {
     return sum(e) - sum(s - 1);
@@ -35,21 +48,22 @@ int main()
     std::cin >> n >> m >> k;
     for (int i = 0; i < n; i++)
     {
-        int num;
+        ll num;
         std::cin >> num;
-        update(i + 1, num);
+        update(i + 1, i + 1, num);
     }
 
+    // 구간 업데이트 및 합 계산을 로그 시간에 수행하는 펜윅 트리
     for (int i = 0; i < m + k; i++)
     {
         int a;
         std::cin >> a;
         if (a == 1)
         {
-            int b, c, d;
+            int b, c;
+            ll d;
             std::cin >> b >> c >> d;
-            for (int i = b; i <= c; i++)
-                update(i, d);
+            update(b, c, d);
         }
         else
         {
