e5f2a0e2024378f961e1d74ced7cbb30c755882d
Wooyoung Myung <mwy3055@gmail.com>
2022-12-06 20:48:06 +0900

    1477. 휴게소 세우기 - 이분 탐색

--- baekjoon/cpp/c++/1477.cpp
+++ baekjoon/cpp/c++/1477.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 
-bool can_divide(std::vector<int> &diff, int m, int l, int mid)
+using vi = std::vector<int>;
+
+bool can_divide(vi &diff, int m, int l, int mid)
 {
     int cnt = 0; // 최대 간격이 mid일 때 세워야 하는 휴게소의 최소 개수
     for (auto &d : diff)
@@ -15,22 +17,27 @@ int main()
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
 
+    // 입력받기
+    // 고속도로의 시작, 끝 지점도 휴게소로 간주 (비어있는 거리 계산을 위해)
     int n, m, l;
     std::cin >> n >> m >> l;
-    std::vector<int> rest(n);
-    for (auto &r : rest)
-        std::cin >> r;
+    vi rest;
+    for (int i = 0; i < n; i++)
+    {
+        int loc;
+        std::cin >> loc;
+        rest.push_back(loc);
+    }
+    rest.push_back(0);
+    rest.push_back(l);
     std::sort(rest.begin(), rest.end());
 
-    std::vector<int> diff;
-    if (rest[0] != 0)
-        diff.push_back(rest[0]);
+    // 휴게소가 없는 구간의 길이 계산
+    vi diff;
     for (int i = 0; i < rest.size() - 1; i++)
         diff.push_back(rest[i + 1] - rest[i]);
-    if (rest.back() != l - 1)
-        diff.push_back(l - 1 - rest.back());
-    std::sort(diff.begin(), diff.end());
 
+    // 휴게소의 최대 간격을 이분 탐색
     int left = 1, right = l; // [left, right]
     while (left < right)
     {
